f9b2fdbf9b8dd88e4e6c5469fe755f8fab4e2123
SuzanaAraujo <suoaraujo@gmail.com>
2023-03-26 15:25:35 -0300

    Ex 3 - componentização e criação do navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ModalComponent } from './modal/modal.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormularioComponent,
+    ModalComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
